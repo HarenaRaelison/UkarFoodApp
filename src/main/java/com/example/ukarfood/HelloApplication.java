76de3be92059e6dabdf1ec18620f90b99073f3e4
HarenaRael <harenaandriahaganirinaraelison@gmail.com>
2023-08-02 09:29:36 +0200

    last commit

--- src/main/java/com/example/ukarfood/HelloApplication.java
+++ src/main/java/com/example/ukarfood/HelloApplication.java
@@ -3,28 +3,21 @@ package com.example.ukarfood;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
-
 import java.io.IOException;
-
 public class HelloApplication extends Application {
-
     @Override
     public void start(Stage stage) throws IOException {
-
-
-
         stage.initStyle(StageStyle.UNDECORATED);
-
-
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-
         stage.setScene(scene);
         stage.show();
+        Image Icon = new Image(getClass().getResource("/images/icons8-fast-food-38.png").openStream());
+        stage.getIcons().add(Icon);
     }
-
     public static void main(String[] args) {
         launch();
     }
