d076311d6be1d25328fd730fd88efbeda3758c47
HarenaRael <harenaandriahaganirinaraelison@gmail.com>
2023-08-02 09:53:44 +0200

    last commit

--- src/main/java/com/example/ukarfood/Controller/MenuController.java
+++ src/main/java/com/example/ukarfood/Controller/MenuController.java
@@ -1,17 +1,19 @@
 package com.example.ukarfood.Controller;
-
+import com.example.ukarfood.HelloApplication;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 import java.io.IOException;
-
 public class MenuController {
-
     @FXML
     private AnchorPane anchorPane;
-
+    @FXML
+    private BorderPane borderPane;
     @FXML
     public void btnFournisseurClick() {
 FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/ukarfood/Fournisseur.fxml"));
@@ -23,7 +25,6 @@ Parent FrnsContent;
         }
 anchorPane.getChildren().setAll(FrnsContent);
     }
-
     @FXML
     public void btnAchatClick(){
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/ukarfood/Achat.fxml"));
@@ -47,15 +48,13 @@ anchorPane.getChildren().setAll(FrnsContent);
     }
     public void btnIngredientClick() {
 FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/ukarfood/Ingredient.fxml"));
-Parent IngredientContent ;
+Parent IngredientContent;
         try {
             IngredientContent = loader.load();
         } catch(IOException e) {
             throw new RuntimeException(e);
         }
         anchorPane.getChildren().setAll(IngredientContent);
-
-
     }
     public void btnHomeClick(){
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/ukarfood/Home.fxml"));
@@ -66,10 +65,7 @@ Parent IngredientContent ;
             throw new RuntimeException(e);
         }
         anchorPane.getChildren().setAll(HomeContent);
-
-
     }
-
     public void AboutUsClick() {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/ukarfood/AboutUs.fxml"));
         Parent AboutUsContent ;
@@ -79,9 +75,19 @@ Parent IngredientContent ;
             throw new RuntimeException(e);
         }
         anchorPane.getChildren().setAll(AboutUsContent);
-
-    }public void initialize(){
+    }public void initialize() throws IOException {
+        HelloApplication obj = new HelloApplication();
+        Stage border = (Stage) borderPane.getScene().getWindow();
+        obj.setIcon(border);
         btnHomeClick();
+
+    }
+    public void Logout(ActionEvent actionEvent) throws IOException {
+        HelloApplication obj = new HelloApplication();
+        Stage primaryStage = new Stage();
+        obj.start(primaryStage);
+        Stage border = (Stage) borderPane.getScene().getWindow();
+        border.close();
     }
 }
 
--- src/main/java/com/example/ukarfood/HelloApplication.java
+++ src/main/java/com/example/ukarfood/HelloApplication.java
@@ -6,16 +6,22 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+
 import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
+
     public void start(Stage stage) throws IOException {
         stage.initStyle(StageStyle.UNDECORATED);
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setScene(scene);
         stage.show();
+
+    }
+    public void setIcon(Stage stage) throws IOException {
         Image Icon = new Image(getClass().getResource("/images/icons8-fast-food-38.png").openStream());
+
         stage.getIcons().add(Icon);
     }
     public static void main(String[] args) {
